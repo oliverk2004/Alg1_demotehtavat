75089afe8bd67666104a574b8b0bfd7bfdb85429
oliverk2004 <oliver.kanden04@gmail.com>
2024-12-09 13:33:18 +0200

    d5 lisätty

---
+++ d5_montakoSanaa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string merkkijono1 = "pallo on pyöreä";
+        string merkkijono2 = "pöydällä on paljon erilaisia kirjoja";
+
+        montakoSanaa(merkkijono1);
+        Console.WriteLine(montakoSanaa(merkkijono2));
+
+    }
+
+
+    public static int montakoSanaa(string merkkijono)
+    {
+        int määrä = 0;
+        string välilyönti = " ";
+
+
+        while (merkkijono.Contains(välilyönti))
+        {
+            määrä += 1;
+        }
+        
+        return määrä;
+    }
+}
+
+
